4e3ea6e5f348d2a527247d31e0ea3607edbd8121
SelimBahcivan <y.selimbahcivan@gmail.com>
2022-06-19 22:16:21 +0300

    initalcreate

---
+++ API/Data/StoreContext.cs
@@ -0,0 +1,10 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+public class StoreContext : DbContext
+{
+    public StoreContext(DbContextOptions options) : base(options)
+    {
+    }
+    public DbSet<Product> Products { get; set; }
+}
