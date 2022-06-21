612989e126c009e88f8e321ea8964ff3b72ac766
SelimBahcivan <y.selimbahcivan@gmail.com>
2022-06-21 14:21:51 +0300

    Added ReactRouter and Axios Configured layout and features

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,50 @@
+import { ContactPage } from '@mui/icons-material';
+import { Container, createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import AboutPage from '../../features/about/AboutPage';
+import Catalog from '../../features/catalog/Catalog';
+import ProductDetails from '../../features/catalog/ProductDetails';
+import HomePage from '../../features/home/HomePage';
+import Header from './Header';
+import './styles.css';
+
+
+function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? 'dark' : 'light'
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+      background: {
+        default: paletteType === 'light' ? 'eaeaea' : '#121212'
+      }
+    }
+  })
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
+  }
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+
+        <CssBaseline />
+
+        <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
+        
+        <Container>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/catalog' element={<Catalog />} />
+            <Route path='/catalog/:id' element={<ProductDetails />} />
+            <Route path='/about' element={<AboutPage />} />
+            <Route path='/contact' element={<ContactPage />} />
+          </Routes>
+        </Container>
+
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default App;
