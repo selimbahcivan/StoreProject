3c86aee92d20714a28220429fcf53ab25c96de16
SelimBahcivan <y.selimbahcivan@gmail.com>
2022-06-22 12:17:46 +0300

    Imported ReactToastify then added into ThemProvider

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -2,12 +2,14 @@ import { ContactPage } from '@mui/icons-material';
 import { Container, createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import AboutPage from '../../features/about/AboutPage';
 import Catalog from '../../features/catalog/Catalog';
 import ProductDetails from '../../features/catalog/ProductDetails';
 import HomePage from '../../features/home/HomePage';
 import Header from './Header';
 import './styles.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() {
@@ -28,10 +30,12 @@ function App() {
     <>
       <ThemeProvider theme={theme}>
 
+        <ToastContainer position='bottom-right' hideProgressBar theme='colored' />
+
         <CssBaseline />
 
         <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
-        
+
         <Container>
           <Routes>
             <Route path='/' element={<HomePage />} />
