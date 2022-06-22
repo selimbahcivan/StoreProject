7ad860f0b30681f2c712f52469577b0f4737b29a
SelimBahcivan <y.selimbahcivan@gmail.com>
2022-06-22 12:18:55 +0300

    Configured AboutPage for testing http errors

--- client/src/features/about/AboutPage.tsx
+++ client/src/features/about/AboutPage.tsx
@@ -1,9 +1,18 @@
-import { Typography } from "@mui/material";
+import { Button, ButtonGroup, Container, Typography } from "@mui/material";
+import agent from "../../app/api/agent";
+
 
 export default function AboutPage() {
     return (
-        <Typography variant='h2'>
-            About page
-        </Typography>
+        <Container>
+            <Typography gutterBottom variant='h2'>Errors for testing purposes</Typography>
+            <ButtonGroup fullWidth>
+                <Button variant='contained' onClick={() => agent.TestErrors.get400Error().catch(error => console.log(error))}>Test 400</Button>
+                <Button variant='contained' onClick={() => agent.TestErrors.get401Error().catch(error => console.log(error))}>Test 401</Button>
+                <Button variant='contained' onClick={() => agent.TestErrors.get404Error().catch(error => console.log(error))}>Test 404</Button>
+                <Button variant='contained' onClick={() => agent.TestErrors.get500Error().catch(error => console.log(error))}>Test 500</Button>
+                <Button variant='contained' onClick={() => agent.TestErrors.getValidationError().catch(error => console.log(error))}>Test Validation error</Button>
+            </ButtonGroup>
+        </Container>
     )
 }
