69228a86ee9a471f79fcc280f514608bd73e5a89
SelimBahcivan <y.selimbahcivan@gmail.com>
2022-06-22 12:09:56 +0300

    Added app.UseMiddleware<ExceptionMiddleware>();

--- API/Program.cs
+++ API/Program.cs
@@ -7,7 +7,6 @@ namespace API
     {
         public static void Main(string[] args)
         {
-            
             var host = CreateHostBuilder(args).Build();
             using var scope = host.Services.CreateScope();
             var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,4 @@
+using API.Middleware;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
@@ -31,9 +32,11 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseMiddleware<ExceptionMiddleware>();
+            
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                // app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
@@ -45,6 +48,8 @@ namespace API
             {
                 opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
             });
+
+            app.UseMiddleware<ExceptionMiddleware>();
 
             app.UseAuthorization();
 
