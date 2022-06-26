39b07205fad399465b090f59e513f752f5a6fc14
SelimBahcivan <y.selimbahcivan@gmail.com>
2022-06-26 19:10:59 +0300

    Added ToastContainer Added Route for ServerError and NotFound

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,16 +1,16 @@
-import { ContactPage } from '@mui/icons-material';
-import { Container, createTheme, CssBaseline, ThemeProvider } from '@mui/material';
-import { useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import AboutPage from '../../features/about/AboutPage';
-import Catalog from '../../features/catalog/Catalog';
-import ProductDetails from '../../features/catalog/ProductDetails';
-import HomePage from '../../features/home/HomePage';
-import Header from './Header';
-import './styles.css';
+import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { useState } from "react";
+import { Route, Switch } from "react-router";
+import { ToastContainer } from "react-toastify";
+import AboutPage from "../../features/about/AboutPage";
+import Catalog from "../../features/catalog/Catalog";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import ContactPage from "../../features/contact/ContactPage";
+import HomePage from "../../features/home/HomePage";
+import Header from "./Header";
 import 'react-toastify/dist/ReactToastify.css';
-
+import ServerError from "../errors/ServerError";
+import NotFound from "../errors/NotFound";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -19,35 +19,32 @@ function App() {
     palette: {
       mode: paletteType,
       background: {
-        default: paletteType === 'light' ? 'eaeaea' : '#121212'
+        default: paletteType === 'light' ? '#eaeaea' : '#121212'
       }
     }
   })
+
   function handleThemeChange() {
     setDarkMode(!darkMode);
   }
+
   return (
-    <>
-      <ThemeProvider theme={theme}>
-
-        <ToastContainer position='bottom-right' hideProgressBar theme='colored' />
-
-        <CssBaseline />
-
-        <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
-
-        <Container>
-          <Routes>
-            <Route path='/' element={<HomePage />} />
-            <Route path='/catalog' element={<Catalog />} />
-            <Route path='/catalog/:id' element={<ProductDetails />} />
-            <Route path='/about' element={<AboutPage />} />
-            <Route path='/contact' element={<ContactPage />} />
-          </Routes>
-        </Container>
-
-      </ThemeProvider>
-    </>
+    <ThemeProvider theme={theme}>
+      <ToastContainer position='bottom-right' hideProgressBar theme='colored' />
+      <CssBaseline />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
+      <Container>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/catalog' component={Catalog} />
+          <Route path='/catalog/:id' component={ProductDetails} />
+          <Route path='/about' component={AboutPage} />
+          <Route path='/contact' component={ContactPage} />
+          <Route path='/server-error' component={ServerError} />
+          <Route component={NotFound} />
+        </Switch>
+      </Container>
+    </ThemeProvider>
   );
 }
 
